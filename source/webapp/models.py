e650f5ae5da7e7acca0b6fbdd68f3aa37cb36b3c
sultaneu <sultanchoponov@gmail.com>
2018-12-27 13:07:52 +0600

    Added model "Employee"

--- source/webapp/models.py
+++ source/webapp/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Employee(models.Model):
+    user = models.OneToOneField(User, on_delete=models.PROTECT, related_name='employee', verbose_name='User')
+    phone = models.CharField(max_length=50, verbose_name='Phone')
+
+    def __str__(self):
+        return self.user.get_full_name()
