9c6025b3d18798b9090b6db2e96948a370bdd826
sultaneu <sultanchoponov@gmail.com>
2018-12-27 13:22:17 +0600

    Added model "Order"

--- source/webapp/models.py
+++ source/webapp/models.py
@@ -17,3 +17,30 @@ class Food(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Order(models.Model):
+    STATUS_NEW = 'new'
+    STATUS_PREPARING = 'preparing'
+    STATUS_ON_WAY = 'on_way'
+    STATUS_DELIVERED = 'delivered'
+    STATUS_CANCELED = 'canceled'
+
+    STATUS_CHOICES = (
+        (STATUS_NEW, 'New'),
+        (STATUS_PREPARING, 'Preparing'),
+        (STATUS_ON_WAY, 'On way'),
+        (STATUS_DELIVERED, 'Delivered'),
+        (STATUS_CANCELED, 'Canceled')
+    )
+
+    contact_phone = models.CharField(max_length=50, verbose_name='Contact Phone')
+    contact_name = models.CharField(max_length=100, verbose_name='Contact Name')
+    delivery_address = models.CharField(max_length=200, null=True, blank=True, verbose_name='Delivery Address')
+    status = models.CharField(max_length=20, default=STATUS_NEW, verbose_name='Status', choices=STATUS_CHOICES)
+    operator = models.ForeignKey(User, null=True, blank=True, related_name='orders', verbose_name='Operator', on_delete=models.PROTECT)
+    courier = models.ForeignKey(User, null=True, blank=True, related_name='delivered', verbose_name='Courier', on_delete=models.PROTECT)
+
+    def __str__(self):
+        return self.contact_phone
+
