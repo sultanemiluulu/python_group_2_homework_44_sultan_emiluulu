d01e8a17df3146661250b7ba12f5d8e8a937de75
sultaneu <sultanchoponov@gmail.com>
2018-12-27 13:48:07 +0600

    Added EmployeeListView, url for it and employee_list.html

--- source/main/urls.py
+++ source/main/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from webapp.views import EmployeeListView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('employee_list', EmployeeListView.as_view(), name='employee_list'),
 ]
--- source/webapp/views.py
+++ source/webapp/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, View, DeleteView
+from django.urls import reverse
+from django.shortcuts import get_object_or_404, redirect
+from django.http import HttpResponseRedirect
+from webapp.models import Food, Order, OrderFood, Employee
+from webapp.forms import FoodForm, OrderForm, OrderFoodForm
 
+
+class EmployeeListView(ListView):
+    model = Employee
+    template_name = 'employee_list.html'
 
